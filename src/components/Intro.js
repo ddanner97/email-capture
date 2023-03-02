5eb0359550fb4668fe3310cb49d1176a05dcfa48
Damian Danner <damian@Damians-iMac.lan>
2023-03-02 11:46:22 -0600

    fix intro styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Intro() {
   return (
-    <div className='intro-header h-screen z-40 flex items-center justify-center animate-pulse duration-2500'>
+    <div className='intro-header h-screen z-40 flex items-center justify-center animate-pulse duration-2500 text-2xl'>
         Welcome to the <br></br> 'Call It What You Want' <br></br> Album Release Part
     </div>
   )
