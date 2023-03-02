8ead3fd555b61eb3f72c68819c34727e133ca56b
Damian Danner <damian@Damians-iMac.lan>
2023-03-02 11:17:54 -0600

    finish artist page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,7 +11,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 function LandingPage({ cancelLandingPage, theme }) {
   return (
     <div className='nav-buttons z-40 relative' style={{ backgroundColor: 'black' }}>
-      <h1 className='text-white text-center mt-2 font-bold underline'>Featured Artists</h1>
+      <h1 className='text-white text-center mt-4 p-4 font-bold underline'>Featured Artists</h1>
 
       <div className='artist-container px-2 py-2 flex flex-col justify-center'>
         <div className='artist-content-container my-2 flex flex-col items-center'>
@@ -19,13 +19,13 @@ function LandingPage({ cancelLandingPage, theme }) {
           <div className='artist-info text-center'>
             <h2 className='text-white'>Lee Samson</h2>
             <div className='artist-links mt-2 flex gap-8'>
-              <a href=''>
+              <a href='https://open.spotify.com/artist/1wtwrLmQXPolEpMvkM7glz?si=2EyW2-OAQVqqGmzktseGXA' target='_blank'>
                 <i class="fa-brands fa-spotify text-white text-2xl"></i>
               </a>
-              <a href=''>
+              <a href='https://music.apple.com/us/artist/lee-samson/1603119541' target='_blank'>
                 <i class="fa-brands fa-apple text-white text-2xl"></i>
               </a>
-              <a href=''>
+              <a href='https://instagram.com/theleesamson?igshid=YmMyMTA2M2Y=' target='_blank'>
                 <i class="fa-brands fa-instagram text-white text-2xl"></i>
               </a>
             </div>
@@ -37,13 +37,13 @@ function LandingPage({ cancelLandingPage, theme }) {
           <div className='artist-info text-center'>
             <h2 className='text-white'>Rarie</h2>
             <div className='artist-links mt-2 flex gap-8'>
-              <a href=''>
+              <a href='https://open.spotify.com/artist/3gx9EoMohBkbHOxcUZwwpo?si=lmlPP2b0RLm5yjrLnkP2xw' target='_blank'>
                 <i class="fa-brands fa-spotify text-white text-2xl"></i>
               </a>
-              <a href=''>
+              <a href='https://music.apple.com/us/artist/rarie/1602880440' target='_blank'>
                 <i class="fa-brands fa-apple text-white text-2xl"></i>
               </a>
-              <a href=''>
+              <a href='https://instagram.com/yourstrulyrarie?igshid=YmMyMTA2M2Y=' target='_blank'>
                 <i class="fa-brands fa-instagram text-white text-2xl"></i>
               </a>
             </div>
@@ -55,13 +55,13 @@ function LandingPage({ cancelLandingPage, theme }) {
           <div className='artist-info text-center'>
             <h2 className='text-white mx-auto'>Darrell Chism</h2>
             <div className='artist-links mt-2 flex gap-8'>
-              <a href=''>
+              <a href='https://open.spotify.com/artist/173jmPStRX93ZR4nh23Utq?si=BQoSwAs1TXaG2ZLM-8Zn1w' target='_blank'>
                 <i class="fa-brands fa-spotify text-white text-2xl"></i>
               </a>
-              <a href=''>
+              <a href='https://music.apple.com/us/artist/darrell-chism/1191962642' target='_blank'>
                 <i class="fa-brands fa-apple text-white text-2xl"></i>
               </a>
-              <a href=''>
+              <a href='https://instagram.com/darrellchism?igshid=YmMyMTA2M2Y=' target='_blank'>
                 <i class="fa-brands fa-instagram text-white text-2xl"></i>
               </a>
             </div>
@@ -74,13 +74,13 @@ function LandingPage({ cancelLandingPage, theme }) {
       <div className='production-container px-2 py-2 flex gap-4 flex-col justify-center text-white pb-10 mb-16'>
         <div className='flex gap-4 mx-auto'>
           <h2>Darrell Chism:</h2>
-          <a href=''>
+          <a href='https://instagram.com/darrellchism?igshid=YmMyMTA2M2Y=' target='_blank'>
             <i class="fa-brands fa-instagram text-white text-2xl"></i>
           </a>
         </div>
         <div className='flex gap-4 mx-auto'>
           <h2>Damian Danner:</h2>
-          <a href=''>
+          <a href='https://instagram.com/ddanner97?igshid=YmMyMTA2M2Y=' target='_blank'>
             <i class="fa-brands fa-instagram text-white text-2xl"></i>
           </a>
         </div>
