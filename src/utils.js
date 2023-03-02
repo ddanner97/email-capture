4a4d22a45c8cf9896c9f3926700e1a27fe6634a5
Damian Danner <damian@Damians-iMac.lan>
2023-03-02 12:23:38 -0600

    delete uneeded function

--- src/utils.js
+++ src/utils.js
@@ -2,20 +2,6 @@ const apiKey = "$2b$10$YtglDrWiRzphuz5Z9vlh.uA/Ak0ZnHcvSIAUZr0y5B5SK.E/KPFbe"
 const collectionId = "6327791ba1610e63862f9268"
 const url = `https://api.jsonbin.io/v3/b`;
 
-export const readData =  async () => {
-
-    const response = await fetch(`https://api.jsonbin.io/v3/b}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Master-Key': `${apiKey}`
-        }
-    })
-
-    return response.json()
-
-}
-
 export const createBin = (data) => {
     
     const response = fetch(url, {
